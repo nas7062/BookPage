441aaef0efc0b0f693bacc1d24c33c88c389191a
10012 <nas7062@naver.com>
2025-02-12 01:35:54 +0900

    add

---
+++ index.js
@@ -0,0 +1,32 @@
+const sliderContainer = document.querySelector("#slider-items");
+const slidesCount = document.querySelectorAll(".slide").length;
+let currentIdx = 1;
+function movingSlider(index) {
+  sliderContainer.style.transition = "transform 0.5s ease";
+  sliderContainer.style.transform = `translateX(-${575 * index}px)`;
+  console.log("moving");
+}
+document.addEventListener("click", (e) => {
+  let index = currentIdx;
+  e.preventDefault();
+  if (e.target.classList.contains("lBtn")) {
+    currentIdx = currentIdx - 1 >= 0 ? currentIdx - 1 : slidesCount - 1;
+  } else if (e.target.classList.contains("rBtn")) {
+    currentIdx = currentIdx + 1 >= slidesCount ? 0 : currentIdx + 1;
+  }
+
+  movingSlider(index);
+});
+const noticeList = ["함께 읽고 나누는 독서로 생활(투표이벤트)", "전체[개선안내] 독서로 서비스 요청사항 기능 개선 안내",
+  "함께 읽고 나누는 독서로 생활(투표이벤트)",
+  "교육디지털원패스 로그인 활용 안내"];
+
+const notice = document.querySelector('#noticeList');
+
+noticeList.forEach((item) => {
+  let list = document.createElement("li");
+  list.innerHTML = `${item}`;
+  notice.appendChild(list);
+  let line = document.createElement("hr");
+  notice.appendChild(line);
+})
